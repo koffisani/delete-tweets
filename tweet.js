30c41b371d1f640e9a59998c31538a92c75fb3dd
Koffi Sani <koffisani@gmail.com>
2018-07-28 15:53:06 +0000

    Fixing Windows and Python 3.7 compatibility

---
+++ tweet.js
@@ -0,0 +1,35 @@
+window.YTD.tweet.part0 = [ {
+  "coordinates" : null,
+  "retweeted" : false,
+  "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+  "entities" : {
+    "hashtags" : [ ],
+    "symbols" : [ ],
+    "user_mentions" : [ {
+      "name" : " ",
+      "screen_name" : "",
+      "indices" : [ "0.0", "12.0" ],
+      "id_str" : "",
+      "id" : ""
+    } ],
+    "urls" : [ ]
+  },
+  "display_text_range" : [ "0.0", "50.0" ],
+  "favorite_count" : "0.0",
+  "in_reply_to_status_id_str" : "",
+  "geo" : null,
+  "id_str" : "",
+  "in_reply_to_user_id" : "",
+  "truncated" : false,
+  "retweet_count" : "0.0",
+  "id" : "",
+  "in_reply_to_status_id" : "",
+  "created_at" : "",
+  "place" : null,
+  "favorited" : false,
+  "full_text" : "",
+  "lang" : "fr",
+  "contributors" : null,
+  "in_reply_to_screen_name" : "",
+  "in_reply_to_user_id_str" : ""
+} ]
